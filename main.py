7181501af4e4a3c81b7ea6aefdba762a15dce566
eadsouza <eadsouza@loyola.edu>
2024-09-25 16:00:21 -0400

    Pushing the code, used Cameron's algorithm to help me, as well as our test cases that we worked on together - Ethan

--- main.py
+++ main.py
@@ -1,8 +1,59 @@
-# Programmers:
-  # Course:  cs-151
-  # Due Date:
-  # Lab Assignment:
-  # Problem Statement:
-  # Data In: hill type, speed
-  # Data Out: points, message depending on points
-  # Credits:
+# Programmers: Ethan and Cameron
+# Course:  CS151
+# Due Date: 10/4/2024
+# Lab Assignment: Lab 03
+# Problem Statement:
+# Data In:
+# Data Out:
+# Credits:
+
+#import math
+import math
+
+
+#inputs for hill type and speed
+hill_type = input('enter hill type ')
+speed = int(input('enter speed '))
+
+#hill type if statments
+if hill_type == 'normal':
+    height = 46
+    points_meter = 2
+    par = 90
+
+elif hill_type == 'large':
+    height = 70
+    points_meter = 1.8
+    par = 120
+
+#error checking
+elif hill_type != 'normal' or hill_type != 'large':
+    hill_type = input('enter hill type ')
+    if hill_type == 'normal':
+        height = 46
+        points_meter = 2
+        par = 90
+
+    elif hill_type == 'large':
+        height = 70
+        points_meter = 1.8
+        par = 120
+
+
+#air time & distance calculations
+air_time = math.sqrt((2*height)/9.8)
+distance = speed*air_time
+
+#point calculation
+points = 60+(distance-par)*points_meter
+
+#outputs
+if points >= 61:
+    print(points, 'great job for doing better than par!')
+elif points < 10:
+    print(points, 'what happened?')
+else:
+    print(points, 'sorry, you didnt go very far.')
+
+
+
